877d44a93f62f503b88757b9b02b0ef168016045
Davies96 <100558009+Davies96@users.noreply.github.com>
2022-03-25 18:10:45 +0000

    Update get_recipe.php

--- basic 2/php/get_recipe.php
+++ basic 2/php/get_recipe.php
@@ -1,14 +1,16 @@
 <?php
-	require 'get_db_path.php';
-	require GET_DB_PATH."/get_db.php";
+	require 'get_db_handle_path.php';
+	require GET_DB_HANDLE_PATH."/get_db_handle.php";
 
-    $conn = get_db();
-	$name = $_GET['name']
-	$stmt =$conn -> prepare("insert into recipe (recipe_name) values(?)"); 
-	$stmt -> execute([$name]);
+    $conn = get_db_handle();
 	
+	$stmt = $conn -> prepare("select * from recipe");
+	$stmt -> execute();
 	
+	echo json_encode($stmt ->fetch(PDO::FETCH_ASSOC));
 	
 	$stmt = null;
 	$conn = null;
+
+
 ?>
