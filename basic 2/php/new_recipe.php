508dbd7fd614910479d180e71ad0b499cbc4b936
Davies96 <100558009+Davies96@users.noreply.github.com>
2022-03-25 18:11:21 +0000

    Update new_recipe.php

--- basic 2/php/new_recipe.php
+++ basic 2/php/new_recipe.php
@@ -1,14 +1,21 @@
-<?php
-	require 'get_db_path.php';
-	require GET_DB_PATH."/get_db.php";
-
-    $conn = get_db();
-	$name = $_GET['name']
-	$stmt =$conn -> prepare("insert into recipe (recipe_name) values(?)"); 
-	$stmt -> execute([$name]);
-	
-	
-	
-	$stmt = null;
-	$conn = null;
-?>
+	<?php
+		require 'get_db_handle_path.php';
+		require GET_DB_HANDLE_PATH."/get_db_handle.php";
+		
+		$conn = get_db_handle();
+		$name = $_REQUEST['name'];
+		$data = $_REQUEST['details']
+		echo $name;
+		$stmt =$conn -> prepare("insert into recipe (recipe_id,recipe_name,vegan,vegetarian,dairy_free) values('201',:name,'1','1','1')"); 
+		$stmt->bindParam(':name',$name);
+		
+		$stmt -> execute();
+		$stmt = null;
+		$stmt =$conn -> prepare("insert into instructions (recipe_id,recipe_name,instructions) values('201',:name,:data)"); 
+		$stmt->bindParam(':name',$name);
+		$stmt->bindParam(':data',$data);
+		$stmt -> execute();
+		
+		$stmt = null;
+		$conn = null;
+	?>
